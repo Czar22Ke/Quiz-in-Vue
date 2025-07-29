3021b494555c3f3e91c8046109cf1cd292f84a4e
Emmanuel Ajayi <santosdemigod12@gmail.com>
2025-07-29 09:54:57 +0100

    Update code sessh

--- src/components/ScoreBoard.vue
+++ src/components/ScoreBoard.vue
@@ -1,8 +1,25 @@
 <template>
-  <div>Score Board Component</div>
+  <section class="score">
+    Player <span>{{}}</span> X <span>{{}}</span> Computer
+  </section>
 </template>
 
 <script>
 export default {};
 </script>
-<style lang="scss"></style>
+<style lang="scss">
+section.score {
+  border-bottom: 1px solid #000;
+  padding: 24px;
+  font-size: 18px;
+
+  span {
+    padding: 8px;
+    margin: 4px;
+    font-weight: bold;
+    border: 1px solid #000;
+  }
+}
+</style>
+
+git add . git commit -m "Update code sessh" git push
